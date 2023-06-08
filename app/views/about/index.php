e56764ebd4af01b3f6ec2ca64681940368e5b96b
muammar200 <amuammar01@gmail.com>
2023-06-08 23:59:01 +0800

    belajar mengelola asset

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    <h1>About Me</h1>
-    <p>Halo nama saya <?=$data['nama'];?>, umur saya <?= $data['umur']?> tahun, saya seorang <?=$data['pekerjaan']?></p>
+    <div class="container">
+        <h1>About Me</h1>
+        <img src="<?=BASEURL;?>/img/neko.jpg" width="200" height="200" class="rounded-circle shadow mb-3 mt-2" alt="Cat Picture">
+        <p>Halo nama saya <?=$data['nama'];?>, umur saya <?= $data['umur']?> tahun, saya seorang <?=$data['pekerjaan']?></p>
+    </div>
--- app/views/template/header.php
+++ app/views/template/header.php
@@ -4,7 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="" rel="stylesheet">
+    <!-- Katanya tidak bisa menggunakan relative path, harus menggunakna absolute path -->
+    <link href="<?= BASEURL; ?>/css/bootstrap.css" rel="stylesheet">
     <title>Halaman <?=$data['judul'];?></title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="<?=BASEURL?>">PHP MVC</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
+        <a class="nav-link" href="<?=BASEURL?>/about">About</a>
+      </div>
+    </div>
+  </div>
+</nav>
